e97913046506f77de0a13d270826db3b5f303aae
Lawtech <584563542@qq.com>
2018-07-11 23:12:02 +0800

    get initial page info

---
+++ crawler/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"golang.org/x/text/transform"
+	"io"
+	"golang.org/x/text/encoding"
+	"golang.org/x/net/html/charset"
+	"bufio"
+)
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	// gbk => utf-8
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+
+}
