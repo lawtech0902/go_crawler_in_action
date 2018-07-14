29bf57be9a6c5b4daa4f5b83ae473d125597064f
Lawtech <584563542@qq.com>
2018-07-14 15:53:04 +0800

    user profile parser

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -10,13 +10,17 @@ const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 func ParseCity(contents []byte) engine.ParseResult {
 	re := regexp.MustCompile(cityRe)
 	matches := re.FindAllSubmatch(contents, -1)
+
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, "User "+string(m[2])) // city
+		name := string(m[2])
+		result.Items = append(result.Items, "User "+string(name))
 		result.Requests = append(result.Requests, engine.Request{
-			Url:        string(m[1]),
-			ParserFunc: engine.NilParser,
-		})                                                        // url
+			Url: string(m[1]),
+			ParserFunc: func(contents []byte) engine.ParseResult {
+				return ParseProfile(contents, name)
+			},
+		})
 	}
 	return result
 }
--- crawler/zhenai/parser/citylist.go
+++ crawler/zhenai/parser/citylist.go
@@ -10,13 +10,20 @@ const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([
 func ParseCityList(contents []byte) engine.ParseResult {
 	re := regexp.MustCompile(cityListRe)
 	matches := re.FindAllSubmatch(contents, -1)
+
 	result := engine.ParseResult{}
+	limit := 10
+
 	for _, m := range matches {
 		result.Items = append(result.Items, "City "+string(m[2])) // city
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
 			ParserFunc: ParseCity,
 		})                                                        // url
+		limit --
+		if limit == 0 {
+			break
+		}
 	}
 	return result
 }
