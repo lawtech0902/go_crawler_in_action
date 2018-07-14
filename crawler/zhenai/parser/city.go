5db5cbcd1b0f4b68f9eae2c8e1ceaf70362333cf
Lawtech <584563542@qq.com>
2018-07-14 13:08:18 +0800

    city parser

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -1,21 +1,21 @@
 package parser
 
 import (
-	"regexp"
 	"go_projects/go_crawler_in_action/crawler/engine"
+	"regexp"
 )
 
-const cityListRe = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 
-func ParseCityList(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityListRe)
+func ParseCity(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityRe)
 	matches := re.FindAllSubmatch(contents, -1)
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, "City "+string(m[2])) // city
+		result.Items = append(result.Items, "User "+string(m[2])) // city
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
-			ParserFunc: ParseCity,
+			ParserFunc: engine.NilParser,
 		})                                                        // url
 	}
 	return result
