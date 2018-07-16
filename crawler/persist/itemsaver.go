0a5a0c1a802a74d34902d5b24d0d114adf1d8daa
Lawtech <584563542@qq.com>
2018-07-16 23:24:45 +0800

    implement itemsaver with elasticsearch

--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -1,6 +1,10 @@
 package persist
 
-import "log"
+import (
+	"log"
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -10,7 +14,25 @@ func ItemSaver() chan interface{} {
 			item := <-out
 			log.Printf("Item Saver: got item #%d: %v", itemCount, item)
 			itemCount ++
+
+			save(item)
 		}
 	}()
 	return out
+}
+
+func save(item interface{}) (id string, err error) {
+	// must turn off sniff in docker
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := client.Index().Index("dating_profile").Type("zhenai").BodyJson(item).Do(context.Background())
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Id, nil
 }
---
+++ crawler/persist/itemsaver_test.go
@@ -0,0 +1,53 @@
+package persist
+
+import (
+	"testing"
+	"go_projects/go_crawler_in_action/crawler/model"
+	"gopkg.in/olivere/elastic.v5"
+	"context"
+	"encoding/json"
+)
+
+func TestSave(t *testing.T) {
+	profile := model.Profile{
+		Name:       "惠儿",
+		Age:        50,
+		Height:     156,
+		Weight:     0,
+		Income:     "3000元以下",
+		Gender:     "女",
+		Xinzuo:     "魔羯座",
+		Marriage:   "离异",
+		Education:  "高中及以下",
+		Occupation: "销售总监",
+		Hukou:      "四川阿坝",
+		House:      "租房",
+		Car:        "未购车",
+	}
+
+	id, err := save(profile)
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := elastic.NewClient(elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Get().Index("dating_profile").Type("zhenai").Id(id).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	t.Logf("%s", resp.Source)
+
+	var actual model.Profile
+	err = json.Unmarshal(*resp.Source, &actual)
+	if err != nil {
+		panic(err)
+	}
+
+	if actual != profile {
+		t.Errorf("got %v; expected %v", actual, profile)
+	}
+}
